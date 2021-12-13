dcec946a1fa3af8f1be510faa1f7fb436998e660
Saigreeshma123 <72641379+Saigreeshma123@users.noreply.github.com>
2021-12-13 14:48:26 +0530

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("****************Enter Number of Snakes*******************");
+        int noOfSnakes = scanner.nextInt();
+        List<Snake> snakes = new ArrayList<Snake>();
+        for (int i = 0; i < noOfSnakes; i++) {
+            snakes.add(new Snake(scanner.nextInt(), scanner.nextInt()));
+        }
+        System.out.println("****************Enter Number of Ladders*******************");
+        int noOfLadders = scanner.nextInt();
+        List<Ladder> ladders = new ArrayList<Ladder>();
+        for (int i = 0; i < noOfLadders; i++) {
+            ladders.add(new Ladder(scanner.nextInt(), scanner.nextInt()));
+        }
+        System.out.println("****************Enter Number of Players*******************");
+        int noOfPlayers = scanner.nextInt();
+        List<Player> players = new ArrayList<Player>();
+        System.out.println("****************Enter Names of Players*******************");
+        for (int i = 0; i < noOfPlayers; i++) {
+            players.add(new Player(scanner.next()));
+        }
+
+        SnakeAndLadderService snakeAndLadderService = new SnakeAndLadderService();
+        snakeAndLadderService.setPlayers(players);
+        snakeAndLadderService.setSnakes(snakes);
+        snakeAndLadderService.setLadders(ladders);
+
+        snakeAndLadderService.startGame();
+        scanner.close();
+    }
+}
